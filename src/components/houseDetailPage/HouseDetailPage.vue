87605a05075b4ebbddba9e0253fc4a7d7480e2ea
Engin Sasmaz <engins.93@gmail.com>
2022-06-22 22:22:09 +0200

    add PrimeVue UI lib

--- src/components/houseDetailPage/HouseDetailPage.vue
+++ src/components/houseDetailPage/HouseDetailPage.vue
@@ -1,3 +1,12 @@
-<template></template>
+<template>
+	<div v-if="houseData" class="house-detail-page">
+		<Image :src="houseData.HoofdFoto" alt="Image Text" />
+	</div>
+</template>
 
 <script src="./houseDetailPage.js"></script>
+
+<style lang="scss">
+.house-detail-page {
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
-import * as Vue from 'vue';
-
+import { createApp } from 'vue';
 import App from './App.vue';
 
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
-const app = Vue.createApp(App);
+import PrimeVue from 'primevue/config';
+import Image from 'primevue/image';
+
+import 'primevue/resources/primevue.min.css';
+
+const app = createApp(App);
+
 app.use(VueAxios, axios);
+app.use(PrimeVue);
+
 app.provide('axios', app.config.globalProperties.axios);
+
+app.component('Image', Image);
+
 app.mount('#app');
