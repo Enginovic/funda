80178be904f27537a56d5e88884305a645745acb
Engin Sasmaz <engins.93@gmail.com>
2022-06-22 20:44:11 +0200

    add axios and add proxy

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,15 @@ const getList = () => {
 	axios
 		.get(
 			`/api/feeds/Aanbod.svc/json/detail/ac1b0b1572524640a0ecc54de453ea9f/koop/2d8cbe85-8111-4a56-92c4-6b6821e9e4df/`
+			// `/api/feeds/Aanbod.svc/ac1b0b1572524640a0ecc54de453ea9f/?type=koop&zo=/amsterdam/tuin/video/`
 		)
-		// .get(`/api/feeds/Aanbod.svc/ac1b0b1572524640a0ecc54de453ea9f/?type=koop&zo=/amsterdam/tuin/video/`)
 		.then((response) => {
 			console.log(response.data);
 		});
 };
 </script>
 
-<template>
-	<img alt="Vue logo" src="./assets/logo.png" />
-</template>
+<template>Hello World.</template>
 
 <style>
 #app {
