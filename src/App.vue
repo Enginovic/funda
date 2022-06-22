32e5372c4d5f7b28c785b4c4b3a1fba0cf70daba
Engin Sasmaz <engins.93@gmail.com>
2022-06-22 20:39:44 +0200

    wip get api response

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { inject, onMounted } from 'vue';
+
+onMounted(() => {
+	getList();
+});
+
+const axios = inject('axios');
+
+const getList = () => {
+	axios
+		.get(
+			`/api/feeds/Aanbod.svc/json/detail/ac1b0b1572524640a0ecc54de453ea9f/koop/2d8cbe85-8111-4a56-92c4-6b6821e9e4df/`
+		)
+		// .get(`/api/feeds/Aanbod.svc/ac1b0b1572524640a0ecc54de453ea9f/?type=koop&zo=/amsterdam/tuin/video/`)
+		.then((response) => {
+			console.log(response.data);
+		});
+};
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<img alt="Vue logo" src="./assets/logo.png" />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import * as Vue from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+const app = Vue.createApp(App);
+app.use(VueAxios, axios);
+app.provide('axios', app.config.globalProperties.axios);
+app.mount('#app');
